76d29e7d76d1e0bd7c34cc2308c06e7128d4d0af
Beomjun-Kim-Econ <beomjunkim.2002@gamil.com>
2024-11-04 21:20:53 +0900

    "Nov 3 2024"

--- Ch3/Ch3-2_LinearRegression.py
+++ Ch3/Ch3-2_LinearRegression.py
@@ -50,8 +50,60 @@ plt.xlabel('length')
 plt.ylabel('weight')
 plt.show()
 
-
 """
 Problem: 흠... 길이가 이웃 3개보다 더 큰데 무게가 이상하다!
 Solution: Linear Regression
 """
+
+from sklearn.linear_model import LinearRegression
+
+# 선형회귀로 훈련!
+lr = LinearRegression()
+lr. fit(train_input, train_target)
+print(lr.predict([[50]]))          # 여러기를 동시에 물을수도 있으므로 array 로 반환
+print(lr.coef_,lr.intercept_)      # 선형회귀의 기울기와 y절편 찾기 ; y_hat = m*x + b
+# # 이 때 lr.coef_ 는 array 형태로 1*n 꼴이다. 왜냐, 다항회귀에서 여러개의 계수를 표현해야하므로!
+
+# 선형회귀 시각화
+plt.scatter(train_input, train_target)    # 훈련세트 산점도
+plt.plot([15, 50], [15*lr.coef_ + lr.intercept_, 50*lr.coef_ + lr.intercept_])
+# [15, 50]: x축 설정,[15*lr.coef_ + lr.intercept_, 50*lr.coef_ + lr.intercept_] 은 두 점을 잇는 직선
+plt.scatter(50, 1241.8, marker='^')       # 50cm 농어 표시
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+print(lr.score(train_input, train_target))
+print(lr.score(test_input, test_target))
+
+"""
+Problem: 보니까 두 점수 너무 낮다. 과소적합된거네. 
+Solution: 항을 추가하자. 다항회귀를 하자!!
+2차 항을 추가하자. 기존 항을 제곱해서 새로운 열을 만들어주면 된다.
+해당 배열을 만들자. 
+"""
+train_poly = np.column_stack((train_input**2, train_input))
+test_poly = np.column_stack((test_input**2, test_input))
+"""
+위 두 줄은 np.column_stack 을 이용하여 어레이를 만든다. 
+이때, np.column_stack 은 여러개의 nx1 배열(column)을 받아 어레이를 리턴한다.
+"""
+print(train_poly.shape, test_poly.shape)
+
+lr_2 = LinearRegression()
+lr_2.fit(train_poly, train_target)        # train_poly의 shape가 n*'2' -> 자동으로 2차식으로 회귀분석해준다.
+print(lr_2.predict([[50**2, 50]]))
+print(lr_2.coef_, lr_2.intercept_)
+# [  1.01433211 -21.55792498] 116.05021078278338 -> 2차항 계수, 1차항 계수, 상수항 값이 각각 1.01, -21, 116이다.
+# 이 때 lr_2.coef_ 는 array 형태로 1*n 꼴이다.
+
+### 다항회귀 시각화 ###
+point = np.arange(15, 50)   # 구간별 직선을 그리기 위해 15에서 49까지의 정수배열 만들기
+plt.scatter(train_input, train_target)    # 훈련세트 산점도
+plt.plot(point, lr_2.coef_[0] * (point**2) + lr_2.coef_[1] * point + lr_2.intercept_)
+# 2차 방정식 그래프, 유저가 정수점을 찍어주면, matplot 이 알아서 곡선으로 바꿔준다.
+plt.scatter(50, 1574, marker='^')       # 50cm 농어 표시
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+print(lr_2.score(train_poly, train_target))
+print(lr_2.score(test_poly, test_target))
